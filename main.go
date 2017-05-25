aa34d1d3c0c63da6ded0905b6fd1630a36c09cd4
ikesato <ikesato76@gmail.com>
2017-05-26 00:55:41 +0900

    add submodules

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"./sub1"
 )
 
 func main() {
 	fmt.Println("hello")
 	fmt.Println(Func1())
 	fmt.Println(func2())
+	fmt.Println(submodule1.Sub1())
+	//fmt.Println(submodule1.sub2()) => Compile Error!
 }
---
+++ sub1/submodule1.go
@@ -0,0 +1,9 @@
+package submodule1
+
+func Sub1() string {
+	return "I'm sub1"
+}
+
+func sub2() string {
+	return "I'm sub2"
+}
